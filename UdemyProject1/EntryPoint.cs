de6b7f7fb81e401979763a72809a075e2c025db6
Kuba <nawrocki90@gmail.com>
2019-03-21 14:13:12 +0100

    CheckBox

--- UdemyProject1/EntryPoint.cs
+++ UdemyProject1/EntryPoint.cs
@@ -5,22 +5,18 @@ using System.Threading;
 class EntryPoint
 {
     static IWebDriver driver = new ChromeDriver();
-    static IWebElement textBox;
+    static IWebElement checkBox;
 
     static void Main()
     {
 
-        string url = "http://testing.todvachev.com/special-elements/text-input-field/";
+        string url = "http://testing.todvachev.com/special-elements/check-button-test-3/";
 
         driver.Navigate().GoToUrl(url);
 
-        textBox = driver.FindElement(By.Name("username"));
-
-        textBox.SendKeys("dupa");
-
-        Thread.Sleep(3000);
-
-        textBox.Clear();
+        checkBox = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=\"checkbox\"]:nth-child(1)"));
+        
+        checkBox.Click();
 
         Thread.Sleep(3000);
 
